61c1d8fcf7c2b6b9db94d19b0da9e88b615fb9d5
Gab <ponce.jgabriela@gmail.com>
2025-07-24 22:25:20 +0200

    update mobile-nav.js
    close menu when option is selected

--- assets/js/mobile-nav.js
+++ assets/js/mobile-nav.js
@@ -7,5 +7,16 @@ document.addEventListener("DOMContentLoaded", function () {
       navUL.classList.toggle("active");
       menuToggle.classList.toggle("active");
     });
+
+    // Cierra el menú cuando se hace clic en un enlace
+    const navLinks = navUL.querySelectorAll("a");
+    navLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        if (navUL.classList.contains("active")) {
+          navUL.classList.remove("active");
+          menuToggle.classList.remove("active");
+        }
+      });
+    });
   }
 });
