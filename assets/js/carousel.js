27a40e1996d51dbda59e75ff5fcd9e13eb185564
Gab <ponce.jgabriela@gmail.com>
2025-07-28 20:08:46 +0200

    update carousel.js
    comments

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -2,15 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const carouselTracks = document.querySelectorAll(".carousel-track");
 
   carouselTracks.forEach((track) => {
-    // Obtenemos los chips originales
     const originalChips = Array.from(track.children);
 
-    // Clonamos los chips varias veces para asegurar que no haya espacios
-    const numberOfClones = 3; // Puedes ajustar este número según sea necesario
+    const numberOfClones = 10;
     for (let i = 0; i < numberOfClones; i++) {
       originalChips.forEach((chip) => {
         const clone = chip.cloneNode(true);
-        clone.setAttribute("aria-hidden", "true"); // Mejora la accesibilidad
+        clone.setAttribute("aria-hidden", "true");
         track.appendChild(clone);
       });
     }
