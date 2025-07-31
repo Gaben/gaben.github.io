5f3a05e73ad6da721d165ec1f15d3d5d22946f83
Gab <ponce.jgabriela@gmail.com>
2025-07-31 14:43:26 +0200

    comments deleted

--- assets/js/autohide-header.js
+++ assets/js/autohide-header.js
@@ -4,15 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let lastScrollY = window.scrollY;
 
-  // Agregamos un umbral para que la barra no desaparezca inmediatamente
   const scrollThreshold = 150;
 
   window.addEventListener("scroll", () => {
     if (lastScrollY < window.scrollY && window.scrollY > scrollThreshold) {
-      // Scroll hacia abajo y hemos pasado el umbral
       header.classList.add("hidden");
     } else {
-      // Scroll hacia arriba
       header.classList.remove("hidden");
     }
     lastScrollY = window.scrollY;
